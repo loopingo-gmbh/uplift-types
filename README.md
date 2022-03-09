30231d1447f7b4b00e6865345f019b4cc280c420
Friedrich Roell <friedrich@roell.com>
2022-03-09 18:22:30 +0100

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+# Summary
+This package contains type definitions for loopingo uplift
+
+# Installation
+> `npm install --save-dev loopingo-gmbh/uplift-types`
+
---
+++ index.d.ts
@@ -0,0 +1,25 @@
+declare namespace loopingo_uplift {
+    function mount(element: HTMLElement)
+    function unmount()
+    function pushEmailAddress(val: string)
+    function pushCartValue(val: number)
+    function pushBasketItem()
+    function completeTransaction(val: string)
+    function setToken(val: string)
+    function setCampaignId(val: string)
+    function onReady(cb: () => void)
+    function maximize()
+    function minimize()
+    function getConfig() : WidgetConfigService
+}
+
+declare interface WidgetConfigService {
+    getActionColor() : string
+    getDangerColor() : string
+    getSuccessColor() : string
+    getThemeColor() : string
+    setThemeColor(value: string) : string
+    setSuccessColor(value: string) : string
+    setActionColor(value: string) : string
+    setDangerColor(value: string) : string
+}
